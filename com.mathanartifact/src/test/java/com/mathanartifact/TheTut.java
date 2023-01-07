a41baaaba1d35ee13753c16ef046b1f3df47a18a
Mtnmathan <mathanmtn27@gmail.com>
2023-01-07 23:37:31 +0530

    sec attempt

---
+++ com.mathanartifact/src/test/java/com/mathanartifact/TheTut.java
@@ -0,0 +1,23 @@
+package com.mathanartifact;
+
+import static org.testng.Assert.assertEquals;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TheTut {
+	public static WebDriver driver;
+	
+	public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	String title = driver.getTitle();
+	System.out.println(title);
+	driver.close();
+	driver.quit();
+	
+	}
+}
